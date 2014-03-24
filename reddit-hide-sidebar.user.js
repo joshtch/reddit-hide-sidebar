113a09b6d8ad618c07bbeb0c3a21cdc13035eb65
wheen- <johnnyischrist@gmail.com>
2014-03-23 17:50:16 -0700

    Update reddit-hide-sidebar.user.js

--- reddit-hide-sidebar.user.js
+++ reddit-hide-sidebar.user.js
@@ -4,6 +4,8 @@
 // @version         0.0.1
 // @author          Wheen
 // @description     Hides the reddit sidebar when the page width is less than 1000 pixels
+// @icon            https://raw.githubusercontent.com/wheen-/reddit-hide-sidebar/master/icon-48x48.png
+// @icon64          https://raw.githubusercontent.com/wheen-/reddit-hide-sidebar/master/icon-64x64.png
 // @domain          reddit.com
 // @domain          www.reddit.com
 // @match           http://*.reddit.com/*
